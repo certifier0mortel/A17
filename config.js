72508bfad66462a76a0a7a1fdce6a36d754d0afb
hacker-debutant <163020888+certifier0mortel@users.noreply.github.com>
2024-04-26 14:32:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ global.openAiAPI = "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";
 global.location = "Tata City, India";
 global.reactmoji = "❤️";
 global.themeemoji = "💖";
-global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
+global.vidmenu = { url: 'https://www.youtube.com/watch?v=FpwrH6rL2Z4' };
 global.websitex = "https://github.com/Kai0071";
 global.lolhuman = "KaysaS";
 
